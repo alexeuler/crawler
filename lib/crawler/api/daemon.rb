b7b6cc1272523797b1d5f7371eaa9019605603e5
alleycat@laptop <karasev.alexey@gmail.com>
2014-03-13 00:59:52 +0400

    changed log dir

--- lib/crawler/api/daemon.rb
+++ lib/crawler/api/daemon.rb
@@ -1,15 +1,15 @@
 require 'daemons'
 require_relative "server"
-dir=File.expand_path("../", File.dirname(__FILE__))
+dir=File.expand_path("../../../", File.dirname(__FILE__))
 
 Daemons.run_proc('api_daemon', {
     dir_mode: :normal,
-    dir: "#{dir}/api/log",
+    dir: "#{dir}/log",
     backtrace: true,
     monitor: true,
     log_output: true
 }) do
-  puts "#{dir}/api/log"
+  puts "#{dir}/log"
   server = Crawler::Api::Server.new
   server.async.start
   sleep
--- lib/crawler/daemon.rb
+++ lib/crawler/daemon.rb
@@ -1,10 +1,10 @@
 require 'daemons'
 require_relative "bot"
-dir=File.expand_path("./", File.dirname(__FILE__))
+dir = File.expand_path("../../", File.dirname(__FILE__))
 
 Daemons.run_proc('bot_daemon', {
     dir_mode: :normal,
-    dir: "#{dir}/api/log",
+    dir: "#{dir}/log",
     backtrace: true,
     monitor: true,
     log_output: true
