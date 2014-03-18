4191490fbc7f60fe84957190c2c4812716d50954
alleycat@laptop <karasev.alexey@gmail.com>
2014-03-19 01:20:28 +0400

    finished post spec

--- spec/crawler/models/post_spec.rb
+++ spec/crawler/models/post_spec.rb
@@ -34,27 +34,24 @@ module Crawler
         end
       end
 
-      #  describe "#fetch_friends" do
-      #    it "fetches friends and saves corresponding UserProfiles in db" do
-      #      user = FactoryGirl.create :user_profile_with_friends
-      #      inv_friend = FactoryGirl.create :user_profile
-      #      user.inverse_friends = [inv_friend]
-      #      @api.should_receive(:friends_get) do
-      #        response = []
-      #        response << UserProfile.all.to_a[1]
-      #        response << UserProfile.last
-      #        response << FactoryGirl.build(:user_profile, vk_id: 999)
-      #        response.map(&:vk_id)
-      #      end
-      #      @api.should_receive(:users_get) do |args|
-      #        args[:uids].should == "999"
-      #        [{uid: 999}]
-      #      end
-      #      user.fetch_friends
-      #      UserProfile.select(:vk_id).to_a.map(&:vk_id).should == [1, 2, 3, 4, 5, 999]
-      #    end
-      #  end
-      #
+      describe "#fetch_likes" do
+        it "fetches user_profiles who liked the post" do
+          post = FactoryGirl.create :post
+          @api.should_receive(:users_get) do |args|
+            args[:uids].should == "1,2,3"
+            [{uid: 1}, {uid: 2}, {uid: 3}]
+          end
+          @api.should_receive(:likes_getList) do |args|
+            args[:owner_id].should == 1
+            args[:item_id].should == 1
+            {users:[1,2,3]}
+          end
+          post.fetch_likes
+          UserProfile.count.should == 3
+          post.likes_user_profiles.should == UserProfile.all.to_a
+        end
+      end
+
     end
 
   end
