06ee4d33a11be839fb7c1f2e883256efb58876b8
alleycat@laptop <karasev.alexey@gmail.com>
2014-03-24 00:58:18 +0400

    constraints in model

--- lib/crawler/models/post.rb
+++ lib/crawler/models/post.rb
@@ -18,7 +18,7 @@ module Crawler
       validates_uniqueness_of :vk_id, scope: :owner_id
 
       belongs_to :user_profile, primary_key: "vk_id", foreign_key: "owner_id"
-      has_many :likes
+      has_many :likes, dependent: :destroy
       has_many :likes_user_profiles, through: :likes, source: "user_profile"
 
       def self.load_or_fetch(id)
